f48d7dd90c6cdf0473de04bbc7a842e093ebcadc
arshekas <singh366arsh@gmail.com>
2022-01-24 03:33:37 -0800

    adding head title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,26 +2,33 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Button } from '@material-ui/core';
 import useWeb3Services from '../../hooks/Web3Services';
+import Head from 'next/head';
 
 function Header() {
   const { account } = useSelector((state) => state.web3);
   const { logout } = useWeb3Services();
 
   return (
-    <header className='main-header'>
-      <h1>Crypto Zilla</h1>
-      <nav className='nav'>
-        <ul>
-          <li>
-            {account && (
-              <Button variant='contained' color='primary' onClick={logout}>
-                Disconnect
-              </Button>
-            )}
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <Head>
+        <title>Crypto Zilla</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
+      <header className='main-header'>
+        <h1>Crypto Zilla</h1>
+        <nav className='nav'>
+          <ul>
+            <li>
+              {account && (
+                <Button variant='contained' color='primary' onClick={logout}>
+                  Disconnect
+                </Button>
+              )}
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
 
